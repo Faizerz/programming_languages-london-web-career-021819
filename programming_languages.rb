9411b8402ce3a776fb95f7cba9a34ebe61ca2b2d
Faiz Abbas <faiz_abbas@hotmail.co.uk>
2019-02-12 10:26:52 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,7 +1,7 @@
 def reformat_languages(languages_by_style)
   new_hash = {}
 
-  languages.each do |language, language_hash|
+  languages_by_style.each do |language, language_hash|
     language_hash.each do |oof, oof_hash|
       oof_hash.each do |script, script_hash|
         script_hash.each do |type_hash|
