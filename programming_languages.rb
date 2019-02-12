22847c58a41f4578f8f9fc8fddd1d59f07dc6fe4
Faiz Abbas <faiz_abbas@hotmail.co.uk>
2019-02-12 10:47:30 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -3,12 +3,10 @@ def reformat_languages(languages_by_style)
 
   languages_by_style.each do |oof, oof_hash|
     oof_hash.each do |language, language_hash|
+      new_hash[language][:style] = []
       language_hash.each do |key, value|
-        if new_hash[language][:style] = nil
-         new_hash[language] = {key => value, style => [oof]}
-       else
-         new_hash[language][:style] << oof
-       end
+        new_hash[language] = {key => value}
+        new_hash[language][:style] << oof
       end
     end
   end
