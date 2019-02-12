676595c13868572d813aa128440e77d0470bafcb
Faiz Abbas <faiz_abbas@hotmail.co.uk>
2019-02-12 10:52:08 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -6,9 +6,11 @@ def reformat_languages(languages_by_style)
       language_hash.each do |key, value|
         new_hash[language] = {key => value}
         new_hash[language][:style] ||= []
-        new_hash[language][:style] << oof
+        new_hash[language][:style].push(oof)
       end
     end
   end
   return new_hash
 end
+
+#a ||= b means, if a is undefined then assign it the value of b, otherwise leave it alone.
